4bfe477c0edadf3a7a7d8156e65c086f8cc18d9f
Camilo Aguilar <camilo.aguilar@gmail.com>
2012-03-13 13:12:04 -0400

    wip

--- runner.js
+++ runner.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 var program = require('commander');
+var uubench = require('uubench');
+var SwarmConn = require('bugswarm-prt').Swarm;
 
 /**
  * This project should show us:
@@ -17,7 +19,74 @@ var program = require('commander');
  **/
 
 program
-    .option('-p, --messages', 'Number of messages to be sent.')
-    .option('-c, --connections', 'Parallel connections.')
+    .option('-c, --connections [number]', 'Number of connections [10]', 10)
+    .option('-m, --messages [number]', 'Number of messages per connection that will be send [100]', 100)
+    .option('-d, --delay [ms]', 'Delay between connections [500]', 500)
+    .option('-t, --timeout [ms]', 'Timeout [1000]', 1000)
+    .option('-a, --credentials [swarm information]', 'Swarm credentials to run the tests, following the format: apikey:resource_id:swarm_id value')
     .parse(process.argv);
+
+//Setting up bugswarm client
+if (!program.credentials) {
+    throw new Error('You must provide your participation key as well as swarm and resource ids in order to start the benchmark');
+}
+var credentials = program.credentials.split(':');
+
+var options = {
+    apikey: credentials[0],
+    resource: credentials[1],
+    swarms: credentials[2]
+};
+
+//Data points to keep track on
+var stats = {
+    connections: {
+        succeeded: 0,
+        failed: 0,
+        fastest: 0, //ms
+        slowest: 0, //ms
+        avg_time_establishment: 0,
+        avg_conn_per_sec: 0,
+        total: 0
+    },
+
+    messages: {
+        sent: 0,
+        received: 0,
+        slowest: 0, //ms
+        fastest: 0, //ms
+        avg_msgs_per_sec: 0,
+        avg_roundtrip_time: 0
+    }
+};
+
+
+var swarmConn = new SwarmConn(options);
+swarmConn.on('error', function(error) {
+    console.log(error);
+    //ignore application errors
+});
+
+var msgs = program.messages;
+var conns = program.connections;
+
+swarmConn.on('message', function(message) {
+    console.log(message);
+});
+
+swarmConn.on('connect', function() {
+    console.log('connected!!');
+    for (var i = 0; i < msgs; i++) {
+        swarmConn.send(' msg number ' + i);
+    }
+});
+
+
+while (conns--) {
+    setTimeout(function() {
+        swarmConn.connect();
+    }, program.delay);
+}
+
+
 
